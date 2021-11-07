6460cb589d4faed61dfc4405324cee9802eea36e
siam3371 <86905091+siam3371@users.noreply.github.com>
2021-11-07 10:37:50 +0600

    Update AuthProvider.js

--- src/Components/Context/AuthProvider.js
+++ src/Components/Context/AuthProvider.js
@@ -1,15 +1,1 @@
- import React, {  createContext } from 'react';
- import useAuth from '../Hooks/useAuth.js';
-//  react router create context
-export const AuthContext = createContext();
-
- const AuthProvider = ({children}) => {
- const allContext = useAuth(); 
-        return ( 
-        <AuthContext.Provider value={allContext}>
-            {children}
-        </AuthContext.Provider> 
-     );
- };
  
- export default AuthProvider;
